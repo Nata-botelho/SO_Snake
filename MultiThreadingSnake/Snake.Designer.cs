50cf84d616c454e5771eea0b8c2808f9db748700
Natã Botelho <nbotelho789@hotmail.com>
2020-12-18 23:02:35 -0300

    random food and head snake point added

---
+++ MultiThreadingSnake/Snake.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace MultiThreadingSnake
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.FoodLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Font = new System.Drawing.Font("Microsoft PhagsPa", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.Location = new System.Drawing.Point(12, 9);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(76, 24);
+            this.ScoreLabel.TabIndex = 0;
+            this.ScoreLabel.Text = "Score: 0";
+            this.ScoreLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FoodLabel
+            // 
+            this.FoodLabel.BackColor = System.Drawing.Color.Red;
+            this.FoodLabel.Location = new System.Drawing.Point(446, 193);
+            this.FoodLabel.Name = "FoodLabel";
+            this.FoodLabel.Size = new System.Drawing.Size(20, 20);
+            this.FoodLabel.TabIndex = 1;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1024, 681);
+            this.Controls.Add(this.FoodLabel);
+            this.Controls.Add(this.ScoreLabel);
+            this.Font = new System.Drawing.Font("Microsoft PhagsPa", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MultiThreadingSnake";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label ScoreLabel;
+        public System.Windows.Forms.Label FoodLabel;
+    }
+}
+
---
+++ MultiThreadingSnake/Snake.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiThreadingSnake
+{
+    public partial class Main : Form
+    {
+        int cols = 50, rows = 30, score = 0, FIRST = 0, nx = 0, ny = 0;
+        bool[,] bodyLocation;
+        Cell[] snake = new Cell[1000];
+
+        Random rand = new Random();
+
+        public Main()
+        {
+            InitializeComponent();
+            GameStart();
+        }
+
+        private void GameStart()
+        {
+            bodyLocation = new bool[rows, cols];
+            Cell head = new Cell((rand.Next() % cols) * 20, ((rand.Next() % rows)+3) * 20);
+
+            for(int i=0; i<rows; i++)
+                for(int j=0; j<cols; j++)
+                    bodyLocation[i, j] = false;
+
+            bodyLocation[(head.Location.Y / 20)-3, (head.Location.X / 20)] = true;
+            snake[FIRST] = head;
+            Controls.Add(head);
+            System.Console.WriteLine("Head: " + head.Location);
+            newFoodLocation();
+        }
+
+        private void newFoodLocation()
+        {
+            int fx, fy;
+            do
+            {
+                fx = rand.Next()%cols;
+                fy = (rand.Next()%rows);
+            } while (bodyLocation[fy, fx]);
+            FoodLabel.Location = new Point(fx*20, (fy+3)*20);
+            System.Console.WriteLine("Food: "+FoodLabel.Location);
+        }
+
+        private void Move()
+        {
+            if (nx == 0 && ny == 0) return;
+
+            int x = snake[FIRST].Location.X;
+            int y = snake[FIRST].Location.Y;
+
+            //if(itselfCrash())
+        }
+
+        private bool itselfCrash(int x, int y)
+        {
+            if(bodyLocation[x, y])
+            {
+                MessageBox.Show("Snake crashed his Body!");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool collectFood(int x, int y)
+        {
+            if (FoodLabel.Location.X == x && FoodLabel.Location.Y == y)
+                return true;
+
+            return false;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
